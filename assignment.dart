d7d2322e056cda555cb2fa6b454cae5966a2a154
KiptooRugut <mccrug97@gmail.com>
2025-06-28 03:34:29 +0300

    Defining variables

---
+++ assignment.dart
@@ -0,0 +1,22 @@
+// Task 1: Define Variables.
+void defineVariables() {
+  // Integer
+  int age = 25;
+  print('Integer: $age');
+  
+  // Double
+  double height = 5.7;
+  print('Double: $height');
+  
+  // String
+  String name = 'Alice';
+  print('String: $name');
+  
+  // Boolean
+  bool isStudent = true;
+  print('Boolean: $isStudent');
+  
+  // List of integers
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('List<int>: $numbers');
+}
